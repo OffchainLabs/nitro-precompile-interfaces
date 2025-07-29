b65a0bdf3ef045bbce72f6095a52a32402935cce
gzeon <im@gzeon.dev>
2025-07-30 01:10:42 +0800

    feat: ChainOwnerRectified

--- ArbOwnerPublic.sol
+++ ArbOwnerPublic.sol
@@ -27,4 +27,6 @@ interface ArbOwnerPublic {
 
     /// @notice Get the infrastructure fee collector
     function getInfraFeeAccount() external view returns (address);
+
+    event ChainOwnerRectified(address rectifiedOwner);
 }
