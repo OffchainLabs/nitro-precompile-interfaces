38135f2be42850186f8b9ed331a8b09d7a3e951f
Mikhail Rogachev <mrogachev@offchainlabs.com>
2025-12-30 11:29:37 +0100

    ArbFilteredTransactionsManager: add events for added/deleted filtered transactions

--- ArbFilteredTransactionsManager.sol
+++ ArbFilteredTransactionsManager.sol
@@ -10,6 +10,12 @@ pragma solidity >=0.4.21 <0.9.0;
  *         Available in ArbOS version 60 and above
  */
 interface ArbFilteredTransactionsManager {
+    /// @notice Emitted when a transaction hash is added to the filtered transactions list.
+    event FilteredTransactionAdded(bytes32 indexed txHash);
+
+    /// @notice Emitted when a transaction hash is removed from the filtered transactions list.
+    event FilteredTransactionDeleted(bytes32 indexed txHash);
+
     /// @notice Marks the given transaction hash as filtered
     /// @param txHash The transaction hash to filter
     function addFilteredTransaction(
